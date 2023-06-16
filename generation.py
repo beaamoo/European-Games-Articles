dfbd1a2eb45019b71be4f5184de1f4d05d634717
ahubler01 <131806293+ahubler01@users.noreply.github.com>
2023-06-16 18:12:18 +0200

    Update generation.py

--- generation.py
+++ generation.py
@@ -1,19 +1,37 @@
 import openai
 import streamlit as st
 
-def gptgeneration(event_df,ranking_df,info):
+#tech lab: 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
+
+def gptgeneration(event_df,ranking_df,info, schedule, date):
 # Set up your OpenAI API credentials
-    openai.api_key = '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'
+    openai.api_key = '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'
 
     # Transform the df into text for the prompt
     # Load your DataFrame (replace with your actual DataFrame)
 
-#Website to generate prompts: https://promptperfect.jina.ai/prompts
-    input_text = f'''Compose a captivating blog article summarizing the medals that were awarded today (latest date) per sport and division, the changes in the overall ranking per country between yesterday and today, and the countries that climbed to the top of the podium and those that were displaced (mention those countries). Talk about the overall ranking exclusively in the end of the article (never mention the ranking before that in any of the other sections). In that section mention the countries that were in the podium the day before, and where they are now after the new medals, same with the ones that were able to get to the podium. also, say how many medals they have and how many medals each country won after today's events. 
-                    Do not write the structure of the article with words such as 'introduction:' or 'conclusion:'. Use titles for each section (sport + division). For each, write a paragraph of less than 100 words, concisely recapping the athletes' outcomes and achievements, the ranking changes, and the podium with the countries with the highest number of medals attributed. 
-                    Discuss the athletes' outcomes and achievements in a concise manner, highlighting their performances and the impact they made in their respective sports. Avoid using overly exaggerated language and focus on providing an engaging and informative recap of the events.{info}'''
-    prompt = """Please regard the following data:\n {}. generate an article that will not have too many expressive words. do not say: incredible, amazing, thrilling,outstanding, remarkable skill, prowess. I do not want such expressiveness. be journalistic professional: {}""".format(event_df, ranking_df, input_text)
+    #Website to generate prompts: https://promptperfect.jina.ai/prompts
+    # Section for the dataframes
+    event_df_section = f"Event Details:\n {event_df}"
+    ranking_df_section = f"\nRanking Details:\n {ranking_df}"
+    schedule_section = f"\nSchedule Details:\n {schedule}"
+
+    # Input text
+    input_text = f'''Compose a blog article for the European games 2023 in Krakow summarizing today's medal awards across different sports and divisions, while bearing in mind today's date: {date}. 
+                    Begin with a succinct overview of yesterday's events. Follow with the main body, divided into sections by sport, discussing athletes' performances, ranking shifts, and medal-winning nations. 
+                    In the conclusion, discuss the overall medal standings. If there's a change in the podium, highlight the countries on yesterday's podium and their positions today, along with the new arrivals. If not, discuss the impacted countries. Limit to the top 5 countries in the rankings.
+                    Avoid using labels like 'Introduction' or 'Conclusion'. Keep the introduction under 150 words, the main body within 600 words, and the conclusion under 150 words. Use a storytelling approach, devoid of excessive expressive words like incredible, amazing, thrilling, outstanding, remarkable skill, or prowess, to present the athletes' achievements. {info}'''
+
+    # Prompt
+    prompt = f'''{event_df_section} 
+                {ranking_df_section} 
+                {schedule_section} 
+                Generate an article that will not have too many expressive words. do not say: incredible, amazing, thrilling,outstanding, remarkable skill, prowess. I do not want such expressiveness. You must be journalistic professional. This is the most important point: 
+                {input_text}'''
+
+    st.title(f"Prompt")
     st.markdown(prompt)
+
     # Call the ChatGPT API
     response = openai.Completion.create(
         engine='text-davinci-003',
@@ -27,6 +45,9 @@ def gptgeneration(event_df,ranking_df,info):
 
     # Retrieve the generated response
     generated_text = response.choices[0].text.strip()
+
+    st.title(f"Article")
     st.markdown(generated_text)
+
 
     return generated_text
