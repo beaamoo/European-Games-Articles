a7913546f1d48aa6671fe6cdfe9be26b937a1a28
Beatrice Mossberg <118908954+beaamoo@users.noreply.github.com>
2023-06-19 12:03:55 +0200

    UpdatedGeneration.py
    Optimised prompt for the generation

--- generation.py
+++ generation.py
@@ -18,17 +18,19 @@ def gptgeneration(event_df,ranking_df,info, schedule, date):
     schedule_section = f"\nSchedule Details:\n {schedule}"
 
     # Input text
-    input_text = f'''Compose a blog article for the European games 2023 in Krakow summarizing today's medal awards across different sports and divisions, while bearing in mind today's date: {date}. 
+    input_text = f'''Compose a blog article for the European games 2023 in Krakow summarising today's medal awards across different sports and divisions, while bearing in mind today's date: {date}. 
                     Begin with a succinct overview of yesterday's events. Follow with the main body, divided into sections by sport, discussing athletes' performances, ranking shifts, and medal-winning nations. 
                     In the conclusion, discuss the overall medal standings. If there's a change in the podium, highlight the countries on yesterday's podium and their positions today, along with the new arrivals. If not, discuss the impacted countries. Limit to the top 5 countries in the rankings.
-                    Avoid using labels like 'Introduction' or 'Conclusion'. Keep the introduction under 150 words, the main body within 600 words, and the conclusion under 150 words. Use a storytelling approach, devoid of excessive expressive words like incredible, amazing, thrilling, outstanding, remarkable skill, or prowess, to present the athletes' achievements. {info}'''
+                    Don’t use labels like 'Introduction' or 'Conclusion'. Keep the introduction under 150 words, the main body within 600 words, and the conclusion under 150 words. Use a journalistic professional storytelling approach, do not use expressive words like incredible, amazing, thrilling, outstanding, remarkable skill, prowess. {info}'''
+
+    metadata_keywords = f''' After the article, provide the metadata for the article (under 150 characters) and 5-10 unique keywords optimised for SEO'''
 
     # Prompt
     prompt = f'''{event_df_section} 
                 {ranking_df_section} 
                 {schedule_section} 
-                Generate an article that will not have too many expressive words. do not say: incredible, amazing, thrilling,outstanding, remarkable skill, prowess. I do not want such expressiveness. You must be journalistic professional. This is the most important point: 
-                {input_text}'''
+                {input_text}
+                {metadata_keywords}'''
 
     st.title(f"Prompt")
     st.markdown(prompt)
